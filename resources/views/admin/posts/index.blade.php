c4a5f30cec2c668c9dcce1bbe74104f098290e37
La-wii <sentilavale@gmail.com>
2021-09-15 23:46:13 +0200

    add category index.blade

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -22,6 +22,7 @@
             <tr>
             <th scope="col">Codice</th>
             <th scope="col">Titolo</th>
+            <th scope="col">Categoria</th>
             <th scope="col">Azioni</th>
             </tr>
         </thead>
@@ -30,6 +31,11 @@
             <tr>
                 <th scope="row">{{ $post->id }} </th>
                 <td>{{ $post->title }}</td>
+                <td>
+                    @if ($post->category)
+		                {{$post->category->name}}
+                    @endif
+                </td>
                 <td>
                     <a href="{{route('admin.posts.show', $post->slug)}}" class="btn btn-primary">Show</a>
                     <a href="{{route('admin.posts.edit', $post->id)}} " class="btn btn-warning">Edit</a>
